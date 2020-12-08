20d26f822f23e9d6d2a8e0652caffbc49a38a945
joshsrai <joshsrai@gmail.com>
2020-12-08 14:40:05 -0500

    added get /new & post positive_posts form and logic

--- app/controllers/positive_posts_controller.rb
+++ app/controllers/positive_posts_controller.rb
@@ -8,6 +8,19 @@ class PositivePostsController <  ApplicationController
         erb :'/positive_posts/new'
     end
 
+    post '/positive_posts' do
+        if !logged_in?
+            redirect '/'
+        else
+            if params[:title] != "" && params[:text] != ""
+                @positive_post = PositivePost.create(title: params[:title], text: params[:text], user_id: current_user.id)
+                redirect "/positive_posts/#{@positive_post.id}"
+            else
+                redirect '/positive_posts/new'
+            end
+        end
+    end
+
 
 
 
