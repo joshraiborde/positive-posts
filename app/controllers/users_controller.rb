97a9a4e24238dbf0a9cd6dce49e4e0e1657cb7e6
joshsrai <joshsrai@gmail.com>
2020-12-08 08:31:45 -0500

    fill out get and login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,23 @@ class UsersController <  ApplicationController
         erb :login
     end
 
+    #login in form, find user, login user(create a session)
+    post '/login' do
+        @user = User.find_by(email: params[:email])
+        if @user.authenticate(params[:password])
+            session[:user_id] = @user.id #logs in user
+            redirect "users/#{@user.id}"
+        else
+
+        end
+    end
+
     get '/signup' do
         
     end
+
+    get '/users/:id' do
+        "users show route"
+    end
+
 end
