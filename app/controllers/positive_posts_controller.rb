70ce969d5517e55967886c2a4c869ce064b4e619
joshsrai <joshsrai@gmail.com>
2020-12-08 19:13:10 -0500

    added positive_posts edit route and link

--- app/controllers/positive_posts_controller.rb
+++ app/controllers/positive_posts_controller.rb
@@ -4,32 +4,30 @@ class PositivePostsController <  ApplicationController
         @positive_posts = PositivePost.all.reverse
     end
 
-    get '/positive_posts/new' do #new entry
+    get '/positive_posts/new' do #new entry form
         erb :'/positive_posts/new'
     end
 
-    post '/positive_posts' do
+    post '/positive_posts' do #create a new entry
         if !logged_in?
             redirect '/'
+        end
+        if params[:title] != "" && params[:text] != ""
+            @positive_post = PositivePost.create(title: params[:title], text: params[:text], user_id: current_user.id)
+            redirect "/positive_posts/#{@positive_post.id}"
         else
-            if params[:title] != "" && params[:text] != ""
-                @positive_post = PositivePost.create(title: params[:title], text: params[:text], user_id: current_user.id)
-                redirect "/positive_posts/#{@positive_post.id}"
-            else
-                redirect '/positive_posts/new'
-            end
+            redirect '/positive_posts/new'
         end
     end
 
+    get '/positive_posts/:id' do #show route
+        @positive_post = PositivePost.find(params[:id])
+        erb :'/positive_posts/show'
+    end
 
-
-
-    
-
-
-
-
-
+    get '/positive_posts/:id/edit' do #render edit form
+        erb :'/positive_posts/edit'
+    end
 
 
 end
