1511861ee0505ee8acd831278522092af08f255e
joshsrai <joshsrai@gmail.com>
2020-12-09 15:52:38 -0500

    register Sinatra::Flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "positive"
+    register Sinatra::Flash
   end
 
   get "/" do
