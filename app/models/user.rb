ffaa535e2a7b39db76086897182541308e908830
joshsrai <joshsrai@gmail.com>
2020-12-09 15:59:21 -0500

    added validations for firstname, lastname, email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
-
     has_secure_password
+    validates :firstname, presence: true
+    validates :lastname, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    
     has_many :positive_posts
+
 end
