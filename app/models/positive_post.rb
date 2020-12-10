fb4fbb3c73198b5416317e0f49c7da1dc7d30320
joshsrai <joshsrai@gmail.com>
2020-12-10 08:30:42 -0500

    formatted created at, so it will display: Wed, Dec 09 2020 5:38.03 PM

--- app/models/positive_post.rb
+++ app/models/positive_post.rb
@@ -1,4 +1,10 @@
 class PositivePost < ActiveRecord::Base
     belongs_to :user
     
+    def formatted_created_at
+        self.created_at.strftime('%a, %b %d %Y %l:%M.%S %p')
+        # created_at.strftime("%A, %b %d %Y %l:%M.%S %p")
+        # created_at.strftime('%a, %d %b %Y %H:%M:%S')
+    end
+
 end
