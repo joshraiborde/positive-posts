67d17f1b1836dd4708fb798a1e8c86056a7e248b
joshsrai <joshsrai@gmail.com>
2020-12-11 21:46:27 -0500

    deleted extra lines

--- app/models/positive_post.rb
+++ app/models/positive_post.rb
@@ -3,8 +3,5 @@ class PositivePost < ActiveRecord::Base
     
     def formatted_created_at
         self.created_at.strftime('%a, %b %d %Y %l:%M.%S %p')
-           # created_at.strftime("%A, %b %d %Y %l:%M.%S %p")
-           # created_at.strftime('%a, %d %b %Y %H:%M:%S')
     end
-
 end
