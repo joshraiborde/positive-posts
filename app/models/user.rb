5925ea23cf0d164ccd6b1fa8b8683501685f24e0
joshsrai <joshsrai@gmail.com>
2020-12-11 21:41:06 -0500

    deleted unneccessary lines

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password
 
-    
+
     validates :firstname, presence: true
     validates :lastname, presence: true
     validates :email, presence: true
